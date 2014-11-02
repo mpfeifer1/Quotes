b08680d75f5cb9206c7919b1d49eb447e4ba7187
mpfeifer <mikepf97@gmail.com>
2014-11-01 23:49:02 -0500

    Moved the quote pictures into quote directory for uniformity

--- RandomQuote.sh
+++ RandomQuote.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 Quote=$(shuf -n 1 ~/Code/Quotes/Quotes.txt)
-notify-send "$Quote" --expire-time=9000 --icon=/home/mpfeifer/Pictures/Icons/Quote.png
+notify-send "$Quote" --expire-time=9000 --icon=/home/mpfeifer/Code/Quotes/Quote.png
